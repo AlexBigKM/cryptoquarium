1bcadd21b6e19297c5660a6dc0c9453d11d55b05
Alex <alexanderbigar@gmail.com>
2021-05-19 12:43:39 +0300

    fix notistack

--- src/theme.js
+++ src/theme.js
@@ -1,12 +1,12 @@
 import { createMuiTheme } from '@material-ui/core';
 
 const theme = createMuiTheme(
-  /** Here you can override default theme values */
-  {
-    spacing: 8,
-  },
-  /** Here you can add your custom values */
-  {},
+    /** Here you can override default theme values */
+    {
+        spacing: 8,
+    },
+    /** Here you can add your custom values */
+    {},
 );
 
 export default theme;
