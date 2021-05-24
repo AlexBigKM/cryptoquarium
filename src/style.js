c22f2611d3476a082fef0fa779016f44cff62c65
Alex <alexanderbigar@gmail.com>
2021-05-24 14:15:28 +0300

    android optimization2

--- src/style.js
+++ src/style.js
@@ -90,6 +90,7 @@ export const useStyles = makeStyles((theme) => ({
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         backgroundAttachment: 'fixed',
+        scrollBehavior: 'smooth',
     },
     mainBar: {
         backgroundColor: '#070B15',
